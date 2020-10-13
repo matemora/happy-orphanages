c0d01841f471f3058dc581018b44e28cb6f1029f
Mateus Morais <mateus.batista010@gmail.com>
2020-10-12 23:43:53 -0300

    initial development: create landing page and maps page

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/global.css';
+import './styles/pages/landing.css'
+import Routes from './Routes';
+function App() {
+  return (
+   <Routes />
+  );
+}
+
+export default App;
